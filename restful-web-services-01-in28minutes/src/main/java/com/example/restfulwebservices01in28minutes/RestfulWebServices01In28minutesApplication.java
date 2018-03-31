098c303cbf2e0d54b6ccc101b36ccd79ca61db38
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-03-30 21:48:30 -0400

    70. Step 18 - Internationalization for RESTful Services

--- restful-web-services-01-in28minutes/src/main/java/com/example/restfulwebservices01in28minutes/RestfulWebServices01In28minutesApplication.java
+++ restful-web-services-01-in28minutes/src/main/java/com/example/restfulwebservices01in28minutes/RestfulWebServices01In28minutesApplication.java
@@ -2,11 +2,32 @@ package com.example.restfulwebservices01in28minutes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServices01In28minutesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebServices01In28minutesApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+
+        return messageSource;
     }
 }
