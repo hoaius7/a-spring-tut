910fb1956f358c7c0f7b69fb41594c78bc442829
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-05-03 08:54:11 -0500

    Add basic security

---
+++ spring-security-demo/src/main/java/com/example/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// add our users for in-memory authentication
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(users.username("duke").password("123456").roles("EMPLOYEE"))
+		.withUser(users.username("maika").password("654321").roles("MANAGER"))
+		.withUser(users.username("xuka").password("135246").roles("ADMIN"));
+		
+	}
+
+	
+}
