62ca7a96ced946f0c92994764a4fa7658efe833a
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-05-03 09:16:49 -0500

    custom login form

--- spring-security-demo/src/main/java/com/example/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/example/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,13 +16,18 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// add our users for in-memory authentication
 		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-		.withUser(users.username("duke").password("123456").roles("EMPLOYEE"))
-		.withUser(users.username("maika").password("654321").roles("MANAGER"))
-		.withUser(users.username("xuka").password("135246").roles("ADMIN"));
-		
+
+		auth.inMemoryAuthentication().withUser(users.username("duke").password("123456").roles("EMPLOYEE"))
+				.withUser(users.username("maika").password("654321").roles("MANAGER"))
+				.withUser(users.username("xuka").password("135246").roles("ADMIN"));
+
 	}
 
-	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated()
+				.and().formLogin().loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser").permitAll();
+	}
+
 }
