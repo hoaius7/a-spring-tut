f0f54ab9be8f94e22c0c23c3f2a5d56816c762b1
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-05-08 09:55:59 -0500

    - Spring Java Config

---
+++ spring-aop-demo/src/main/java/com/example/springaop/demo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.example.springaop.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.example.springaop.demo")
+public class DemoConfig {
+
+}
