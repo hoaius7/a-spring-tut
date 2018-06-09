47f9a9daae68e3fddf388768b02b379ba03c8d24
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-06-09 15:18:17 -0400

    Enhancing the Hello World Service to return a Bean

--- restful-web-service/src/main/java/com/example/restfulwebservices/HelloWorldController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,5 @@
 package com.example.restfulwebservices;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +10,10 @@ public class HelloWorldController {
   @GetMapping(path = "/hello-world")
   public String helloWorld() {
     return "Hello World!";
+  }
+
+  @GetMapping(path = "/hello-world-bean")
+  public HelloWorldBean helloWorldBean() {
+    return new HelloWorldBean("Hello World!");
   }
 }
