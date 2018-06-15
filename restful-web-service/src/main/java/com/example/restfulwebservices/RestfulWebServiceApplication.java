5ace17ca4a8b5e5034604bc479d3199f65a62fa4
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-06-15 09:18:50 -0400

    Internationalization for RESTful Services

--- restful-web-service/src/main/java/com/example/restfulwebservices/RestfulWebServiceApplication.java
+++ restful-web-service/src/main/java/com/example/restfulwebservices/RestfulWebServiceApplication.java
@@ -1,7 +1,11 @@
 package com.example.restfulwebservices;
 
+import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
@@ -9,4 +13,18 @@ public class RestfulWebServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(RestfulWebServiceApplication.class, args);
   }
+  
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.US);
+    return localeResolver;
+  }
+  
+//  @Bean
+//  public ResourceBundleMessageSource bundleMessageSource() {
+//    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//    messageSource.setBasename("messages");
+//    return messageSource;
+//  }
 }
