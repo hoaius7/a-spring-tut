cab8c0757c0bd549c84177a1c3ee19b336e41036
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-06-15 20:16:17 -0400

    Fixed Internationalization for RESTful Services

--- restful-web-service/src/main/java/com/example/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservices/helloworld/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.restfulwebservices.helloworld;
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -32,7 +33,8 @@ public class HelloWorldController {
 
   @GetMapping(path = "/hello-world-internationalized")
   public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-    return messageSource.getMessage("good.morning.message", null, locale);
+//    return messageSource.getMessage("good.morning.message", null, locale);
+    return messageSource.getMessage("good.morning.message", null, "Default Hello!", LocaleContextHolder.getLocale());
   }
   
 }
