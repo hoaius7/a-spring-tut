5129fbc98673ef82c74a57b410bf009b38099efc
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-06-22 21:08:21 -0400

    Step 16 - Configure JPA and Initialized Data

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values (10001, 'USD', 'VND', 21000, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values (10002, 'EUR', 'VND', 28000, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values (10003, 'AUD', 'VND', 16000, 0);
