f4fa8fed65609dbf970392b65fffc1bff9a11bf2
Hoai Nguyen <hoaius7@gmail.com>
2019-05-14 12:10:38 -0500

    new config for config server

---
+++ microservices/m2-configserver/src/main/java/com/example/m2configserver/M2ConfigserverApplication.java
@@ -0,0 +1,15 @@
+package com.example.m2configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class M2ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2ConfigserverApplication.class, args);
+	}
+
+}
